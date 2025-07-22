4abc75267a6dfe518a4e408f3f001737143adb6d
M-Khan517 <113201585+M-Khan517@users.noreply.github.com>
2025-07-22 10:56:30 +0330

    Delete account_module directory

--- account_module/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountModuleConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.account_module"
-    verbose_name = "کاربران"
